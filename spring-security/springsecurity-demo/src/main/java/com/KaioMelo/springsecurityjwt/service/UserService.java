6014c7607f32d346dbd94767824104eea87c8bf3
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2023-11-30 19:43:09 -0300

    Movendo repositório spring-mvc e spring-security

---
+++ spring-security/springsecurity-demo/src/main/java/com/KaioMelo/springsecurityjwt/service/UserService.java
@@ -0,0 +1,21 @@
+package com.KaioMelo.springsecurityjwt.service;
+
+import com.KaioMelo.springsecurityjwt.model.User;
+import com.KaioMelo.springsecurityjwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private PasswordEncoder encoder;
+    public void createUser(User user){
+        String pass = user.getPassword();
+        //criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
+}
